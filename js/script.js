9114f7beef7aa2fd83de50270942bce14956b190
DylanBlanco <dylanblanco1198@gmail.com>
2024-08-02 18:18:53 +0200

    click prev / click next

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//array images e console.log
 const images = [
     {
         image: 'img/01.webp',
@@ -22,7 +23,6 @@ const images = [
     }
 ];
 
-const active = document.getElementById(`active`);
 for (let i = 0; i < images.length; i++) {
     //console.log(`Name`, images[i]);
 
@@ -36,3 +36,48 @@ for (let i = 0; i < images.length; i++) {
         Text
         `, images[i].text);
 };
+
+//carica img dinamicamente
+for (let i = 0; i < images.length; i++) {
+    const img = document.createElement(`img`);
+    img.classList.add(`img`);
+    if (i == 0) {
+        img.innerHTML += `<img src="${images[i]}" class="active">`;
+    }
+    else {
+        img.innerHTML += `<img src="${images[i]}">`;
+    }
+}
+
+const allImgs = document.querySelectorAll(`.box-img > img`);
+console.log(`allImgs`, allImgs, typeof allImgs);
+
+let activeImg = 0;
+
+// click next
+const next = document.getElementById(`next`);
+next.addEventListener(`click`, function () {
+    if (activeImg < images.length - 1) {
+        console.log(`click next`);
+
+        activeImg++;
+    }
+    else {
+        activeImg = 0;
+    }
+    allImgs[activeImg].classList.add(`active`);
+});
+
+// click prev
+const prev = document.getElementById(`prev`);
+prev.addEventListener(`click`, function () {
+    if (activeImg < images.length - 1) {
+        console.log(`click prev`);
+
+        activeImg--;
+    }
+    else {
+        activeImg = 0;
+    }
+    allImgs[activeImg].classList.add(`active`);
+});
